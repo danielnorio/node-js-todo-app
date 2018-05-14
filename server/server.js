00bb64971bffdaab44026d967e54be2af23485cb
danielnorio <danieltakasu@gmail.com>
2018-05-14 16:59:10 -0300

    Add GET /todos/:id with test cases

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -42,6 +42,22 @@ app.get('/todos', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
+});
+
+// GET /todos/123432
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 module.exports = {app};
