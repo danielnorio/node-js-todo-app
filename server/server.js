40b4614c9e02c8844dcaf254b8458ad49aaba14f
danielnorio <danieltakasu@gmail.com>
2018-05-14 13:50:01 -0300

    First commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// Custom middleware to catch body-parser errors
+// to control error response.
+app.use(function (error, req, res, next) {
+  if (error instanceof SyntaxError) {
+    console.log("Erro de sintaxe JSON.", error);
+    res.status(error.statusCode).send(error.toString());
+  } else {
+    next();
+  }
+});
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -19,6 +30,15 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((docs) => {
+    res.send(docs);
+  }, (e) => {
+    res.status(400).send(e);
+    console.log('Error returning todos.', e);
+  })
 });
 
 app.listen(3000, () => {
